efa59410f1648f376d1c18f3acb533f4dd88aa69
usmanFrontDev <usmanrajput4488@gmail.com>
2025-07-21 15:47:42 +0500

    fix: fix testing issue

--- src/constant/Constant.jsx
+++ src/constant/Constant.jsx
@@ -1,6 +1,6 @@
 // import portfolioImg from "../assets/Images/project-1-img.png";
-import pepsiImg from "../assets/images/pepsi-image.png";
 import devoidsImg from "../assets/images/devoids-image.png";
+import pepsiImg from "../assets/images/pepsi-image.png";
 import portfolioImg from "../assets/images/portfolio-img.png";
 import equipmentImg from "../assets/images/equipment-image.png";
 import manelinkImg from "../assets/images/manelink-image.png";
